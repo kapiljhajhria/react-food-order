5d792dad1eb080923896843c919e3669569aaf85
kapil jhajhria <jhajhria44@gmail.com>
2021-05-05 18:31:03 +0530

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
-import CartContext from "./store/cart-context";
 import CartProvider from "./store/CartProvider";
 
 function App() {
