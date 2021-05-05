62d1fa40ff5907c4b7686ff52e54e9f7045848b7
kapil jhajhria <jhajhria44@gmail.com>
2021-05-05 15:44:30 +0530

    close modal on clicking backdrop or close btn

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      {cartIsShown && <Cart />}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
